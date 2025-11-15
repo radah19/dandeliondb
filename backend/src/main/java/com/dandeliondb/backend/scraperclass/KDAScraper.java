daba884a5f69218ff4e0b77952ef6e418b96fff0
Anuj Adulla <anuj.adulla@gmail.com>
2025-11-15 17:54:48 -0500

    Image retrieval added

--- backend/src/main/java/com/dandeliondb/backend/scraperclass/KDAScraper.java
+++ backend/src/main/java/com/dandeliondb/backend/scraperclass/KDAScraper.java
@@ -4,7 +4,12 @@ import com.dandeliondb.backend.model.Product;
 import lombok.AllArgsConstructor;
 import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
+import org.springframework.core.io.Resource;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.RestTemplate;
 
+import java.io.InputStream;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Objects;
@@ -18,6 +23,7 @@ public class KDAScraper {
     }
 
     public List<Product> scrape(Document doc) {
+
         scrapeProduct(doc);
         return ls;
     }
@@ -55,7 +61,7 @@ public class KDAScraper {
                     .select("span:is(.tagged_as) > a");
 
             List<String> tags = new ArrayList<>();
-            if (tagsContainer != null && tagsContainer.size() > 0) {
+            if (tagsContainer != null && !tagsContainer.isEmpty()) {
                 tags.addAll(tagsContainer.removeFirst() // Remove "Tags:" entry
                         .stream().map(Element::text).toList());
             }
@@ -89,7 +95,7 @@ public class KDAScraper {
                     .select(":is(#tab-description) > p");
             List<String> descriptions = new ArrayList<>();
             if (descriptionsContainer != null) {
-                descriptions = descriptionsContainer.stream().map(Element::text).toList();
+                descriptions = new ArrayList<>(descriptionsContainer.stream().map(Element::text).toList());
                 while (descriptions.size() > 1) { // Current select will grab a list of <p> elements. We want to combine it into one description.
                     String p = descriptions.removeFirst();
                     descriptions.set(0, p + "\n" + descriptions.getFirst());
@@ -97,8 +103,32 @@ public class KDAScraper {
             }
 
             Product prod = new Product(title, brand, price, tags, upc, sku, ean, weight, descriptions);
-
             ls.add(prod);
+
+            // Grab Images and convert them to input streams
+            List<InputStream> images = new ArrayList<>();
+            List<Element> imagesContainer = doc
+                    .select("figure:is(.woocommerce-product-gallery__wrapper) > div");
+
+            if (imagesContainer != null && !imagesContainer.isEmpty()) {
+                for (Element e: imagesContainer) {
+                    String imgSrc = e.select("img").attr("src");
+                    System.out.println(imgSrc);
+
+                    RestTemplate restTemplate = new RestTemplate();
+                    ResponseEntity<Resource> response = restTemplate.exchange(
+                            imgSrc,
+                            HttpMethod.GET,
+                            null,
+                            Resource.class
+                    );
+                    Resource ret = response.getBody();
+                    InputStream inputStream = (ret != null) ? ret.getInputStream() : null;
+
+                    Thread.sleep(5000); // 5 second delay to prevent overwhelming the server
+                }
+            }
+
         } catch (Exception e) {
             System.out.println(e.getMessage());
         }
