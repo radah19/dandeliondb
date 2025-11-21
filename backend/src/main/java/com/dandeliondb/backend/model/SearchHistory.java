ceb55d0e03eb7df99c87bcaa8a6c4f8433085595
tarun <tarunramji@gmail.com>
2025-11-21 13:01:20 -0500

    added search history repo

--- backend/src/main/java/com/dandeliondb/backend/model/SearchHistory.java
+++ backend/src/main/java/com/dandeliondb/backend/model/SearchHistory.java
@@ -4,6 +4,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbBean;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbPartitionKey;
+import software.amazon.awssdk.enhanced.dynamodb.mapper.annotations.DynamoDbSortKey;
 
 @AllArgsConstructor
 @NoArgsConstructor
@@ -14,4 +16,14 @@ public class SearchHistory {
     private String timestamp;
     private String name;
     private String brand;
+
+    @DynamoDbPartitionKey
+    public String getEmail() {
+        return email;
+    }
+
+    @DynamoDbSortKey
+    public String getTimestamp() {
+        return timestamp;
+    }
 }
