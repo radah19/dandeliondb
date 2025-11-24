550030e6312fb6943535cf0b932cd99216019b46
Anuj Adulla <anuj.adulla@gmail.com>
2025-11-23 22:48:54 -0500

    Removed bad package name

--- backend/src/main/java/com/dandeliondb/backend/config/RedisConfig.java
+++ backend/src/main/java/com/dandeliondb/backend/config/RedisConfig.java
@@ -1,4 +1,4 @@
-package com.example.redis.config;
+package com.dandeliondb.backend.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
