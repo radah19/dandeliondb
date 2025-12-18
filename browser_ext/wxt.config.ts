e52c433822daa2404a012f0231f437def47788fd
Anuj Adulla <anuj.adulla@gmail.com>
2025-12-18 17:56:00 -0500

    Removed identity perms from browser ext

--- browser_ext/wxt.config.ts
+++ browser_ext/wxt.config.ts
@@ -4,7 +4,7 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    permissions: ['tabs', 'activeTab', 'webNavigation', 'storage', 'identity', 'identity.email'],
+    permissions: ['tabs', 'activeTab', 'webNavigation', 'storage'],
     host_permissions: ['*://*/*'],
     icons: {
       16: '/icon/dandeliondb_flower.png',
@@ -15,7 +15,7 @@ export default defineConfig({
     },
     description: "This extension allows you to integrate DandelionDB's autofill features into your IMS/storepage tech solutions",
     name: "DandelionDB",
-    version: "1.0.0"
+    version: "1.0.1"
   },
   runner: {
     disabled: true  // Disable auto-opening browser
