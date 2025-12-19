0bed2285352de781f066b9c0fdfa55121c27bc6f
Anuj Adulla <anuj.adulla@gmail.com>
2025-12-19 16:30:37 -0500

    Update wxt.config.ts

--- browser_ext/wxt.config.ts
+++ browser_ext/wxt.config.ts
@@ -4,8 +4,8 @@ import { defineConfig } from 'wxt';
 export default defineConfig({
   modules: ['@wxt-dev/module-react'],
   manifest: {
-    permissions: ['tabs', 'activeTab', 'webNavigation', 'storage'],
-    host_permissions: ['*://*/*'],
+    permissions: ['tabs', 'webNavigation', 'storage'],
+    host_permissions: ['*://*.mybigcommerce.com/*','*://*.lightspeedapp.com/*'],
     icons: {
       16: '/icon/dandeliondb_flower.png',
       32: '/icon/dandeliondb_flower.png',
@@ -15,7 +15,7 @@ export default defineConfig({
     },
     description: "This extension allows you to integrate DandelionDB's autofill features into your IMS/storepage tech solutions",
     name: "DandelionDB",
-    version: "1.0.1"
+    version: "1.0.5"
   },
   runner: {
     disabled: true  // Disable auto-opening browser
