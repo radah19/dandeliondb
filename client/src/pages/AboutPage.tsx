7694300df4a274d72aa47846bb0bb905e8651393
Anuj Adulla <anuj.adulla@gmail.com>
2025-12-20 13:40:59 -0500

    raaa

--- client/src/pages/AboutPage.tsx
+++ client/src/pages/AboutPage.tsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react';
-import { useNavigate, useLocation } from 'react-router-dom';
+import { useLocation } from 'react-router-dom';
 import './AboutPage.css';
 
 function AboutPage() {
